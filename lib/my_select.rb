4a406810ece34596e60cff0c8db2f4b564614484
Kara O <karajo.odle@gmail.com>
2019-05-21 21:45:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  
+  while i < collection.length do
+    
+    yield(collection[i])
+    
+    i = i + 1 
+    
+  end
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
 end
